c602a9c91b1ac82af939315ca79101addfaf8ff8
samo2105 <samuel.cea95@gmail.com>
2020-01-19 20:12:59 -0300

    added yargs

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        }
+    }).argv;
+
 const {crearArchivo} = require('./multiplicar/multiplicar');
 
-if (process.argv[2]){
+console.log(argv.base)
+/*if (process.argv[2]){
     let base = process.argv[2];
     if(!Number(base)){
-        console.log(`El valor ${base} no es un número`)
+        console.log(`El valor ${base} no es un número`);
         return
     }
-}
+}*/
 
+/*
 crearArchivo(base)
     .then(archivo => console.log(`El archivo ${archivo} ha sido creado`))
-    .catch(e => console.log(e));
+    .catch(e => console.log(e));*/
